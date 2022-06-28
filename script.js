f4d51f22bd9b19ec1f5c32c6cb4cca1eed00e11b
Iyimide Adekile <105992164+iyifr@users.noreply.github.com>
2022-06-28 23:32:21 +0100

    Starting To work on the js file

---
+++ script.js
@@ -0,0 +1,13 @@
+const musicContainer = document.querySelector('.music-container')
+const playBtn = document.querySelector('#play')
+const prev = document.querySelector('#prev')
+const next = document.querySelector('next')
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
+const title = document.querySelector('#title')
+const cover = document.querySelector('#cover')
+
+//Song Titles
+const songs = ["Nikes" , "Ivy" , "Pink + White", "Be Yourself", "Solo", "Skyline To", "Self Control" , "Good Guy",  "Nights" , "Solo(Reprise)"
+"Pretty Sweet" ,"Facebook Story" , "Close to You" . "White Ferrari", "Seigfried", "Godspeed", "Futura Free" ]                                     ]
