ed4b702d08354739e5c739dcb9038cfa30a789aa
Iyimide Adekile <105992164+iyifr@users.noreply.github.com>
2022-06-29 00:44:57 +0100

    error handled

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const musicContainer = document.querySelector('.music-container')
 const playBtn = document.querySelector('#play')
 const prev = document.querySelector('#prev')
-const next = document.querySelector('next')
+const next = document.querySelector('#next')
 const audio = document.querySelector('#audio')
 const progress = document.querySelector('.progress')
 const progressContainer = document.querySelector('.progress-container')
@@ -9,5 +9,21 @@ const title = document.querySelector('#title')
 const cover = document.querySelector('#cover')
 
 //Song Titles
-const songs = ["Nikes" , "Ivy" , "Pink + White", "Be Yourself", "Solo", "Skyline To", "Self Control" , "Good Guy",  "Nights" , "Solo(Reprise)"
-"Pretty Sweet" ,"Facebook Story" , "Close to You" . "White Ferrari", "Seigfried", "Godspeed", "Futura Free" ]                                     ]
+const songs = ["Nikes" , "Ivy" , "Pink + White", "Be Yourself", "Solo", "Skyline To", "Self Control" , "Good Guy",  "Nights" , "Solo (Reprise)",
+"Pretty Sweet" ,"Facebook Story" , "Close to You" , "White Ferrari", "Seigfried", "Godspeed", "Futura Free" ] 
+console.log(songs)                                    
+
+//Song Index
+let songIndex = 12
+
+//Initially Load Song into DOM
+loadSong(songs[songIndex])
+
+
+//Update song details
+ function loadSong(song) {
+ 	title.innerText = song
+ 	audio.src = `Music/${song}.mp3`
+ 	cover.src = "images/blonde.jpg"
+
+ }
