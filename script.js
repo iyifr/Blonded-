703e6d32905c2ac1dba78d485e8bec547b4154c7
Iyimide Adekile <105992164+iyifr@users.noreply.github.com>
2022-06-29 14:06:06 +0100

    Update script.js
    Project Completed , i learnt new js methods and using the browsers audio Api, it was really fun

--- script.js
+++ script.js
@@ -71,6 +71,16 @@ function updateProgress(e) {
  const {duration, currentTime} = e.srcElement
  const progressPercent = (currentTime / duration) * 100
  progress.style.width = `${progressPercent}%`
+
+}
+
+function setProgress(e) {
+	const width = this.clientWidth
+	const clickX = e.offsetX
+	const duration = audio.duration
+
+	audio.currentTime= (clickX / width) * duration
+
 }
 
  //Event Handlers
@@ -93,3 +103,7 @@ prevBtn.addEventListener("click", prevSong)
 nextBtn.addEventListener("click", nextSong)
 
 audio.addEventListener('timeupdate' , updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener("ended", nextSong)
