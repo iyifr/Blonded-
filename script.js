5391a2f5d717c59f79135affc3398918f1058127
Iyimide Adekile <105992164+iyifr@users.noreply.github.com>
2022-06-29 01:22:50 +0100

    added audio

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const songs = ["Nikes" , "Ivy" , "Pink + White", "Be Yourself", "Solo", "Skyline
 console.log(songs)                                    
 
 //Song Index
-let songIndex = 12
+let songIndex = 3
 
 //Initially Load Song into DOM
 loadSong(songs[songIndex])
@@ -27,3 +27,37 @@ loadSong(songs[songIndex])
  	cover.src = "images/blonde.jpg"
 
  }
+
+
+ function playSong() {
+ 	musicContainer.classList.add('play')
+ 	playBtn.querySelector("i.fas").classList.remove('fa-play')
+ 	playBtn.querySelector("i.fas").classList.add('fa-pause')
+
+
+ 	audio.play()
+ }
+
+ function pauseSong() {
+ 	musicContainer.classList.remove('play')
+ 	playBtn.querySelector("i.fas").classList.remove('fa-pause')
+ 	playBtn.querySelector("i.fas").classList.add('fa-play')
+
+ 	audio.pause()
+ }
+
+ //Event Handlers
+ playBtn.addEventListener("click", ()=> {
+ 	const isPlaying = musicContainer.classList.contains('play')
+ 	if (isPlaying) {
+ 		pauseSong();
+
+ 	}
+
+ 	else {
+ 		playSong()
+ 	}
+ })
+
+
+ //Change Song Events
